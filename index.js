363acda082f91d7c3633a64f5af8d047f2e0fc18
Alexandre Eletherio de Brito Junior <eletherioalexandre@gmail.com>
2021-12-02 14:38:47 -0300

    V1.0 do Projeto Finalizada

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const express = require('express');
-const {engine} = require('express-handlebars');
 const bodyParser = require('body-parser');
 const alocacao = require('./models/alocacao');
 
 const app = express();
 
-//Config
-  //Template Engine
-      //const exphbs = h.create({
-      //  extname: '.hbs',
-      //defaultLayout: "main" });
-      app.engine('handlebars', engine());
-      app.set('view engine', 'handlebars');
-      app.set('views', './views');
-      handleb.registerPartials(__dirname + '/views/partials');
-
-
-
-
 //Body Parser Config
       app.use(bodyParser.urlencoded({extended: false}));
       app.use(bodyParser.json());
 
+
+      app.use(express.static("./public"));
+      
+
 //Rotas
 app.get('/', (req, res) => {
-  res.render('formulario');
+  res.render('home');
 });
 
 app.post('/cadastro', (req, res) => {
@@ -39,12 +29,10 @@ app.post('/cadastro', (req, res) => {
     qualificacao: req.body.qualificacao
   })
   .then(() => {
-    res.redirect('/');
-   // alert("Cadastro efetuado com sucesso!");
+    res.redirect('/home');
   })
   .catch(() => {
     res.send("ERRO ao finalizar cadastro. Por favor, tente novamente mais tarde!")
   });
 });
-
 app.listen(3000, () => {console.log("O servidor está no ar!")});
